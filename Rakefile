ea7ce2ffd0647d9d88f907795c59b5c560b031b4
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-09-17 16:32:55 +0900

    Add: Setup Rubocop

--- Rakefile
+++ Rakefile
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 require "bundler/gem_tasks"
+require "syntax_tree/rake_tasks"
 task default: %i[]
+
+SyntaxTree::Rake::CheckTask.new
+SyntaxTree::Rake::WriteTask.new
--- lib/rubocop/service.rb
+++ lib/rubocop/service.rb
@@ -4,7 +4,8 @@ require_relative "service/version"
 
 module Rubocop
   module Service
-    class Error < StandardError; end
+    class Error < StandardError
+    end
     # Your code goes here...
   end
 end
